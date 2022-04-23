694a5486f0761aec6d2a9975f443a9e1b42b1220
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-04-23 18:25:57 +0530

    [Madhavee] Add method to validate user's password for rule 1

--- UserRegistrationProblem/Validation.cs
+++ UserRegistrationProblem/Validation.cs
@@ -90,5 +90,26 @@ namespace UserRegistrationProblem
                 return false;
             }
         }
+        /// <summary>
+        /// Password Rule 1 Validation
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns>bool</returns>
+        public static bool PasswordRule1Validation(string password) 
+        {
+            //Rule -1 Minimum 8 characters
+            string patternForRule1 = "^[0-9a-zA-Z@#$%^&*!+=]{8,}"; // Regex for password validation rule 1
+            //If password entered by user is match with regex then it is valid otherwise not
+            if (Regex.IsMatch(password, patternForRule1))
+            {
+                Console.WriteLine($"\nYour password \"{password}\" is valid");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"\nYour password \"{password}\" is not follow password rules");
+                return false;
+            }
+        }
     }
 }
